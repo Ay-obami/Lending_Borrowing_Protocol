86261290a9b953648a990db257b5ed2aced9d9a3
Ay-obami <oyebisiayobami26@email.com>
2025-10-16 15:55:26 +0100

    openzepplin submodule tracking reactivated

--- src/Vtokens/DUsdt.sol
+++ src/Vtokens/DUsdt.sol
@@ -25,7 +25,7 @@
 
 pragma solidity ^0.8.0;
 
-import {ERC20Burnable, ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Burnable, ERC20} from "src/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Dusdt is ERC20Burnable, Ownable {
