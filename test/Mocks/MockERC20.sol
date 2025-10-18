084d640179819b7adfc20cb6b5bae9f25de1f365
Ay-obami <oyebisiayobami26@email.com>
2025-10-18 14:47:48 +0100

    .

---
+++ test/Mocks/MockERC20.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier-MIT
+
+/// @dev Simple mintable ERC20 used in tests when not running on Sepolia
+contract MockERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    uint256 public totalSupply;
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        uint256 allowed = allowance[from][msg.sender];
+        if (msg.sender != from) {
+            require(allowed >= amount, "Allowance");
+            allowance[from][msg.sender] = allowed - amount;
+        }
+        require(balanceOf[from] >= amount, "Insufficient");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        return true;
+    }
+
+    function mint(address to, uint256 amount) external {
+        balanceOf[to] += amount;
+        totalSupply += amount;
+    }
+}
