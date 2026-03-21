f890cb49e21edd7cbe58f4f4b148d550c3ffef67
Ay-obami <oyebisiayobami26@email.com>
2026-03-21 17:12:26 +0100

    Borrowing, collateralMananger contract refactored to use interestCalculator contract

--- src/core/CollateralManager.sol
+++ src/core/CollateralManager.sol
@@ -1,50 +1,79 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {InterestCalculator} from "./InterestCalculator.sol";
 
 contract CollateralManager {
     using SafeERC20 for IERC20;
 
     IERC20 public collateralToken;
+    InterestCalculator public supplyInterestCalculator;
+    address public liquidationManager;
+    uint256 public constant RAY = 1e18;
+    uint256 public totalScaledDeposits;
+
     mapping(address => uint256) public collateralBalances;
     mapping(address => uint256) public lockedCollateralBalances;
 
-    constructor(address _collateralToken) {
+    constructor(address _collateralToken, address _liquidationManager) {
         collateralToken = IERC20(_collateralToken);
+        liquidationManager = _liquidationManager;
+        supplyInterestCalculator = new InterestCalculator(0.1e18, 0.8e18, 0.1e18, 0.2e18, 0.1e18, true);
     }
 
-    function deposit(uint256 amount) external {
+    function deposit(uint256 amount, uint256 utilizationRate) external {
+        supplyInterestCalculator.updateLiquidityIndex(utilizationRate);
+        uint256 liquidityIndex = supplyInterestCalculator.getLiquidityIndex();
+        uint256 scaledAmount = amount * RAY / liquidityIndex;
         collateralToken.safeTransferFrom(msg.sender, address(this), amount);
-        collateralBalances[msg.sender] += amount;
+        collateralBalances[msg.sender] += scaledAmount;
+        totalScaledDeposits += scaledAmount;
     }
 
-    function withdraw(uint256 amount) external {
-        require(collateralBalances[msg.sender] >= amount, "Insufficient balance");
-        collateralBalances[msg.sender] -= amount;
+    function withdraw(uint256 amount, uint256 utilizationRate) external {
+        supplyInterestCalculator.updateLiquidityIndex(utilizationRate);
+        uint256 liquidityIndex = supplyInterestCalculator.getLiquidityIndex();
+        uint256 scaledAmount = amount * RAY / liquidityIndex;
+        require(collateralBalances[msg.sender] >= scaledAmount, "Insufficient balance");
+        collateralBalances[msg.sender] -= scaledAmount;
+        totalScaledDeposits -= scaledAmount;
         collateralToken.safeTransfer(msg.sender, amount);
     }
 
-    function forceWithdraw(uint256 amount) external {
-        // Force withdraw collateral (used in liquidation)
-        require(lockedCollateralBalances[msg.sender] >= amount, "Insufficient locked collateral");
-        lockedCollateralBalances[msg.sender] -= amount;
+    function forceWithdraw(address borrower, uint256 amount, uint256 utilizationRate) external {
+        require(msg.sender == liquidationManager, "Not authorized");
+        supplyInterestCalculator.updateLiquidityIndex(utilizationRate);
+        uint256 liquidityIndex = supplyInterestCalculator.getLiquidityIndex();
+        uint256 scaledAmount = amount * RAY / liquidityIndex;
+        require(lockedCollateralBalances[borrower] >= scaledAmount, "Insufficient locked collateral");
+        lockedCollateralBalances[borrower] -= scaledAmount;
+        totalScaledDeposits -= scaledAmount;
         collateralToken.safeTransfer(msg.sender, amount);
     }
 
     function lockCollateral(uint256 amount) external {
-        // Lock collateral in the pool
+        require(collateralBalances[msg.sender] >= amount, "Insufficient collateral");
         collateralBalances[msg.sender] -= amount;
         lockedCollateralBalances[msg.sender] += amount;
     }
 
     function unlockCollateral(uint256 amount) external {
-        // Unlock collateral in the pool
         require(lockedCollateralBalances[msg.sender] >= amount, "Insufficient locked collateral");
         lockedCollateralBalances[msg.sender] -= amount;
         collateralBalances[msg.sender] += amount;
     }
-}
 
+    function getUserBalance(address user, uint256 utilizationRate) external view returns (uint256) {
+        return supplyInterestCalculator.getBalance(collateralBalances[user], utilizationRate);
+    }
+
+    function getPoolBalance() public view returns (uint256) {
+        return collateralToken.balanceOf(address(this));
+    }
+
+    function getTotalDepositsWithInterest(uint256 utilizationRate) external view returns (uint256) {
+        return supplyInterestCalculator.getBalance(totalScaledDeposits, utilizationRate); // fix: use scaled total
+    }
+}
